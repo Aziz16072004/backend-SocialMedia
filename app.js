5b38e1463419158e6fcd07c7613c42111ce53db1
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-04 20:38:51 +0100

    updating

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const cors = require("cors")
 const app = express();
 app.use(cors())
 app.use(express.json())
-app.get("/", (req, res) => {
-    res.send("helloooo1234");
-});
 app.use("/" , require("./routes/homeRoute"))
 app.use("/home" , require("./routes/productRoute"))
 
@@ -15,17 +12,19 @@ app.use("/home" , require("./routes/productRoute"))
 mongoose.set("strictQuery" , false);
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGO_URI);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  }
-
+}
 
 const PORT = process.env.PORT || 8000
-
+app.get("/", (req, res) => {
+    res.send("helloooo1234"+PORT);
+});
+//Connect to the database before listening
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
