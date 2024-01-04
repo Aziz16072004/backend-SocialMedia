3cec2898d4d9767bf98306b6455c32f90c16df49
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-04 16:37:38 +0100

    updating

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("helloooo");
 });
-// app.use("/" , require("./routes/homeRoute"))
-// app.use("/home" , require("./routes/productRoute"))
+app.use("/" , require("./routes/homeRoute"))
+app.use("/home" , require("./routes/productRoute"))
 
 
 
-
-// mongoose.connect('mongodb://127.0.0.1:27017/data');
+mongoose.set("strictQuery" , false);
+const connectDB = async ()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDb Connected : ${conn.connection.host}`);
+    }catch(error)
+    {
+        console.log(error);
+        process.exit(1)
+    }
+}
+mongoose.connect('mongodb://127.0.0.1:27017/data');
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
