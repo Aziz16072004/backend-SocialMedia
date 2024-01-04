56a8904ba4ced61dc8548ba9c556de92ba478802
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-04 15:55:09 +0100

    updating

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/home" , require("./routes/productRoute"))
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/data');
-
-app.listen(8000, () => {
-    console.log(`Server started on http://localhost:8000`);
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 });
