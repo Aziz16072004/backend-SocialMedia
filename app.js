0977946ed8364184990e4b607e9db6d96646c05e
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-04 16:09:42 +0100

    updating

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.send("helloooo");
 });
-app.use("/" , require("./routes/homeRoute"))
-app.use("/home" , require("./routes/productRoute"))
+// app.use("/" , require("./routes/homeRoute"))
+// app.use("/home" , require("./routes/productRoute"))
 
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/data');
+// mongoose.connect('mongodb://127.0.0.1:27017/data');
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
