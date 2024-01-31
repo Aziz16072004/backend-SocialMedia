0443fd299bf8b216213f1f9329bea18365bbe872
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-31 18:10:54 +0100

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/message" , require("./routes/messageRoute"))
 mongoose.set("strictQuery" , false);
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/data");
+        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/socialMediaProject");
         console.log(`MongoDB Connected: ${conn.connection.host} `);
     } catch (error) {
         console.log(error);
