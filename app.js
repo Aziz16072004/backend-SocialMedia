19e0ee23e4cc3554e27c3f8658ac38e3e59c309e
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-31 17:53:18 +0100

    hebergement

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT ||8000;
 const server = http.createServer(app) 
 const io = new Server(server, {
     cors: {
-        origin: ["https://blushing-train-newt.cyclic.app"],
+        origin: ["http://localhost:3000","http://localhost:3001"],
         credentials: true
     }
 });
